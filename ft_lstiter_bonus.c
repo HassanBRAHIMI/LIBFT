ba8b9ec5439d95c7446e03ec37dc14c7bed9b680
HassanBRAHIMI <brahimihassan054@gmail.com>
2024-01-09 10:02:17 +0100

    added

--- ft_lstiter_bonus.c
+++ ft_lstiter_bonus.c
@@ -6,7 +6,7 @@
 /*   By: hbrahimi <hbrahimi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 12:44:27 by hbrahimi          #+#    #+#             */
-/*   Updated: 2024/01/08 21:51:18 by hbrahimi         ###   ########.fr       */
+/*   Updated: 2024/01/09 10:01:44 by hbrahimi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,27 +20,5 @@ void	ft_lstiter(t_list *lst, void (*f)(void *))
 	{
 		f(lst->content);
 		lst = lst->next;
-	}
-}
-void f(void *kda)
-{
-	free(kda);
-}
-#include <stdio.h>
-int main()
-{
-
-	t_list *node1 = ft_lstnew(ft_strdup("aaa"));
-	t_list *node2 = ft_lstnew(ft_strdup("bbb"));
-	t_list *node3 = ft_lstnew(ft_strdup("ccc"));
-	t_list *head = NULL;	
-	ft_lstadd_back(&head, node1);
-	ft_lstadd_back(&head, node2);
-	ft_lstadd_back(&head, node3);
-	ft_lstiter(head, f);
-	while (head)
-	{
-		printf("%s\n",head->content);
-		head = head->next;
 	}
 }
